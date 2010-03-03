5d4fe2b80dd131319a10a6c150669a9d38bb3d73
VanL <van.lindberg@gmail.com>
2010-03-03 15:34:50 +0000

    The biblion settings.py references a non-existent BIBLION_SECTIONS variable. Fixed that by giving it a default value.

--- biblion/settings.py
+++ biblion/settings.py
@@ -1,6 +1,13 @@
 from django.conf import settings
 
+DEFAULT_SECTION_CHOICES = [
+        ("1", "business"),
+        ("2", "technical"),
+        ("3", "general"),
+    ]
 
 
 ALL_SECTION_NAME = getattr(settings, "BIBLION_ALL_SECTION_NAME", "all")
-SECTIONS = settings.BIBLION_SECTIONS
+SECTIONS = getattr(settings, "BIBLION_SECTIONS", DEFAULT_SECTION_CHOICES)
+
+#~
